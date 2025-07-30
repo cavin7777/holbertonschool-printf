c9b46ef49613168d81f08ae6424e8b3deb400c13
cavin7777 <caven7777@gmail.com>
2025-07-30 13:40:53 +0400

    printf

--- prinf_test.c
+++ prinf_test.c
@@ -5,26 +5,54 @@
 /**
  * _printf - A simplified printf function
  * @format: The format string
- * Return: Number of characters printed
+ * Return: Numbers of characters printed
  */
 int _printf(const char *format, ...)
 {
 	int i;
+	int count = 0;
+	va_list args;
 
-		for (i = 0; format[i] != '\0'; i++)
+	if (format == NULL)
+		return (-1);
 
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
 		{
-			if (format[i] != '%')
-				putchar(format[i]);
-
+			putchar(format[i]);
+			count++;
 		}
-		return (0);
-}
+		else
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
 
+				putchar(c);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s = va_arg(args, char *);
 
-int main(void)
-{
-	int printed = _printf("Hello, Lucas!!!!!!\n");
-
-	return (0);
+				while (*s)
+				{
+					putchar(*s++);
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				putchar('%');
+				putchar(format[i]);
+				count += 2;
+			}
+		}
+	}
+	va_end(args);
+	return (count);
 }
