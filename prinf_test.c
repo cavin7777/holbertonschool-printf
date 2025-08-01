b4da728a38b09819443d579cf9738945f5622d10
cavin7777 <caven7777@gmail.com>
2025-08-01 10:33:40 +0400

    printf

--- prinf_test.c
+++ prinf_test.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * _printf - A simplified printf function
@@ -52,6 +53,44 @@ int _printf(const char *format, ...)
 					putchar(*s);
 					s++;
 					count++;
+				}
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				int n = va_arg(args, int);
+				char buf[20];
+				int j = 0;
+				int neg = 0;
+
+				if (n == 0)
+				{
+					write(1, "0", 1);
+					count++;
+				}
+				else
+				{
+					if (n < 0)
+					{
+						neg = 1;
+						n = -n;
+					}
+
+					while (n > 0)
+					{
+						buf[j++] = (n % 10) + '0';
+						n /= 10;
+					}
+
+					if (neg)
+					{
+						buf[j++] = '-';
+					}
+
+					while (j--)
+					{
+						write(1, &buf[j], 1);
+						count++;
+					}
 				}
 			}
 			else if (format[i] == '%')
